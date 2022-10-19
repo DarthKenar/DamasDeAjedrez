95e5fc1a1da19ae352dc1adc9f70543f609db035
DarthKenar <federico_vega22@hotmail.com>
2022-10-19 01:11:27 -0300

    Continua el error, avance sobre bucles en funcion (comprobación), aplicado list_comprehension en la creacion de tablero

--- main.py
+++ main.py
@@ -21,34 +21,73 @@ def comprobacion (tablero, cant_damas):
 
     soluciones = 0
     damas_en_tablero = 0
-    tablero_copia = []
+    tablero_copia = tablero.copy()
+
+    #Debería agregar doble bucle anidado? para realizar nuevamente la comprobacion cuando no encuentre seolucion?
 
     for i in range(0, cant_damas):
 
         for j in range(0, cant_damas):
-            
-            if tablero[i][j] == 0:
 
-                tablero_copia = tablero.copy()
+            # i representada por el valor Y del eje de coordenadas
+            # j representada por el valor X del eje de coordenadas
+
+            if tablero_copia[i][j] == 0:
+
+                
 
                 tablero_copia[i][j] = "D"
                 damas_en_tablero += 1
                 
                 #3
-                for movimiento_horizontal in range(j,cant_damas):
+                for movimiento_X in range(j,cant_damas):
 
-                    tablero_copia[i][j + movimiento_horizontal] = 1
-                    
+                    tablero_copia[i][movimiento_X] = 1
+
+                #4.5
+                for movimiento_X in range(j, cant_damas):
+                    for movimiento_Y in range(i, cant_damas):
+
+                        tablero_copia[movimiento_Y][movimiento_X] = 1
                 
+                #6
+                for movimiento_Y in range(i, cant_damas):
+
+                    tablero_copia[movimiento_Y][j] = 1
                 
+                #7.5 (good)
+                for movimiento_X in range(j-1, 0, -1):
+                    for movimiento_Y in range(i+1, cant_damas):
+                        
+                        tablero_copia[movimiento_Y][movimiento_X] = 1
                 
-            else:
-                if cant_damas == damas_en_tablero:
-                    
-                    tablero_solucion = tablero_copia.copy()
-                    soluciones += 1
-                else:
-                    continue
+                #9
+                for movimiento_X in range(j-1, 0, -1):
+
+                    tablero_copia[i][movimiento_X] = 1
+                
+                #10.5
+                for movimiento_X in range(j-1, 0, -1):
+                    for movimiento_Y in range(i-1, 0, -1):
+
+                        tablero_copia[movimiento_Y][movimiento_X] = 1
+                
+                #12
+                for movimiento_Y in range(i-1, 0, -1):
+
+                    tablero_copia[movimiento_Y][j] = 1
+
+                #1.5
+                for movimiento_X in range(j, cant_damas):
+                    for movimiento_Y in range(i-1, 0, -1):
+
+                        tablero_copia[movimiento_Y][movimiento_X] = 1
+
+
+    if cant_damas == damas_en_tablero:
+        
+        tablero_solucion = tablero_copia.copy()
+        soluciones += 1
     
     if soluciones > 0:
         mostrar_tablero(tablero_solucion, cant_damas)
@@ -57,21 +96,10 @@ def comprobacion (tablero, cant_damas):
         print(f"No se encontraron soluciones para posicionar {cant_damas} damas en un tablero de {cant_damas}x{cant_damas}")
 
 cant_damas = int(input("Ingrese la cantidad de damas: "))
-lista = []
-tablero = []
-#tablero_copia = []
 
-#Creacion de tablero
-for i in range(0,cant_damas):
+#Creacion de tablero inicializado en 0 utilizando la técnica list_comprehension
+tablero = [[0 for i in range(cant_damas)]for j in range(cant_damas)]
 
-    print("Gola")
-    lista.append(0)
-    for j in range(0,cant_damas):
-
-        tablero.append(lista)
-
-
-
+#prueba para visualización de datos
 mostrar_tablero(tablero, cant_damas)
-tablero[2][2] = "1"
-mostrar_tablero(tablero, cant_damas)
+comprobacion(tablero,cant_damas)
