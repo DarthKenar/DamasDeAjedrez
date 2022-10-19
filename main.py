426bb55d54cc5cad60bc7b94284f2acb7b4ae70c
DarthKenar <federico_vega22@hotmail.com>
2022-10-19 00:04:57 -0300

    Antes de la implemntacion de numpy, las matrices no estan funcionando correctamente en este punto

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def movimiento_12 (tablero, cant_damas):
+
+    for i in range(tablero, cant_damas):
+
+        for j in range(tablero, cant_damas):
+
+            if tablero[i][j] == "D":
+                continue############
+
 def mostrar_tablero (tablero, cant_damas):
 
     for i in range(0, cant_damas):
@@ -10,38 +19,59 @@ def mostrar_tablero (tablero, cant_damas):
 
 def comprobacion (tablero, cant_damas):
 
+    soluciones = 0
     damas_en_tablero = 0
+    tablero_copia = []
+
     for i in range(0, cant_damas):
 
         for j in range(0, cant_damas):
-        
+            
             if tablero[i][j] == 0:
 
-                tablero[i][j] = "D"
+                tablero_copia = tablero.copy()
+
+                tablero_copia[i][j] = "D"
                 damas_en_tablero += 1
-                #Introducir valor 1 en casillas donde ataque esta dama
-                for q in range(i)
+                
+                #3
+                for movimiento_horizontal in range(j,cant_damas):
+
+                    tablero_copia[i][j + movimiento_horizontal] = 1
+                    
+                
+                
                 
             else:
-                continue
-            
+                if cant_damas == damas_en_tablero:
+                    
+                    tablero_solucion = tablero_copia.copy()
+                    soluciones += 1
+                else:
+                    continue
+    
+    if soluciones > 0:
+        mostrar_tablero(tablero_solucion, cant_damas)
+        print(f"La cantidad de soluciones posibles es: {soluciones}")
+    else:
+        print(f"No se encontraron soluciones para posicionar {cant_damas} damas en un tablero de {cant_damas}x{cant_damas}")
 
 cant_damas = int(input("Ingrese la cantidad de damas: "))
 lista = []
 tablero = []
+#tablero_copia = []
 
 #Creacion de tablero
 for i in range(0,cant_damas):
 
     print("Gola")
     lista.append(0)
+    for j in range(0,cant_damas):
 
-for j in range(0,cant_damas):
+        tablero.append(lista)
 
-    tablero.append(lista)
+
 
 mostrar_tablero(tablero, cant_damas)
-
-
-
-
+tablero[2][2] = "1"
+mostrar_tablero(tablero, cant_damas)
